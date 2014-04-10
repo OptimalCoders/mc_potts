70932db2984c53b1da2505c2958217103cf54c21
greschd <greschd@ethz.ch>
2014-04-10 12:08:41 +0200

    33. Commit:	changing baseline_greschd to new iface (incomplete) greschd
    DONE:
    - split baseline_greschd into sim, grid and matrix (incomplete)
    
    TODO:
    - fix it
    - move energy/magn-density calculation to sim

---
+++ src/grid/baseline_greschd_grid.hpp
@@ -0,0 +1,86 @@
+// Author:  Dominik Gresch <greschd@ethz.ch>
+// Date:    10.04.2014 01:11:35 CEST
+// File:    baseline_greschd_impl.hpp
+
+#ifndef __BASELINE_GRESCHD_impl_HEADER
+#define __BASELINE_GRESCHD_impl_HEADER
+
+
+#include <alpha/baseline_impl_greschd/global.hpp>
+
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <math.h>
+
+namespace mc_potts {
+    
+    struct baseline_greschd_grid {
+        
+        template<int L1, int L2, int L3, typename MATRIX>
+        class impl {
+        //------------------------local typedefs----------------------------//
+            typedef uint16_t spin_rep_t;
+            typedef std::vector<std::vector<std::vector<spin_rep_t>>> system_t;
+        
+        public:
+        //------------------------public typedefs---------------------------//
+            typedef int index_type;
+            
+        //------------------------Constructor-------------------------------//
+            impl(): system_() {};
+        
+        //------------------------get - set---------------------------------//
+            
+            inline spin_ret_type const & get(index_type const & x, index_type const & y, index_type const & z) const {
+                return system_.get(x, y, z);
+            }
+            
+            spin_ret_type get_nn(index_type const & x, index_type const & y, index_type const & z) const {
+                return  get((x - 1 + L1) % L1, y, z) + get((x + 1) % L1, y, z) +
+                        get(x, (y - 1 + L2) % L2, z) + get(x, (y + 1) % L2, z) +
+                        get(x, y, (z - 1 + L3) % L3) + get(x, y, (z + 1) % L3);
+            }
+            
+            void set(index_type const & x, index_type const & y, index_type const & z, spin_ret_type const & s) {
+                system_.set(x, y, z, s);
+            }
+            
+        //------------------------observables-------------------------------//
+        
+            double magn_density() const {
+                spin_ret_type res = 0;
+                for(index_type i = 0; i < L1; ++i) {
+                    for(index_type j = 0; j < L2; ++j) {
+                        for(index_type k = 0; k < L3; ++k) {
+                            res += get(i, j, k);
+                        }
+                    }
+                }
+                return res / (L1 * L2 * L3) - (S - 1) / 2.;
+            }
+            
+            double energy_density() const {
+                double res = 0;
+                for(index_type i = 0; i < L1; ++i) {
+                    for(index_type j = 0; j < L2; ++j) {
+                        for(index_type k = 0; k < L3; ++k) {
+                            res -= (get(i, j, k) - (S - 1) / 2.) * (get_nn(i, j, k) - 6 * (S - 1) / 2. );
+                        }
+                    }
+                }
+                return res * baseline_greschd::Jh / (2. * L1 * L2 * L3);
+            }
+            
+
+        private:
+        //------------------------private members---------------------------//
+            typename MATRIX::template impl<L1, L2, L3> system_;
+            
+        };
+    }; // struct baseline_greschd_impl 
+    
+} // namespace mc_potts
+
+
+#endif //__BASELINE_GRESCHD_impl_HEADER
---
+++ src/matrix/baseline_greschd_matrix.hpp
@@ -0,0 +1,47 @@
+// Author:  Dominik Gresch <greschd@ethz.ch>
+// Date:    10.04.2014 01:20:18 CEST
+// File:    baseline_greschd_matrix.hpp
+
+#ifndef ___HEADER
+#define ___HEADER
+
+#include <types.hpp>
+
+namespace mc_potts {
+    using mc_potts::index_type;
+    struct baseline_greschd_matrix {
+        
+        typedef uint8_t spin_rep_t;
+        typedef std::vector<std::vector<std::vector<spin_rep_t>>> system_t;
+        
+        template<   index_type L1, 
+                    index_type L2, 
+                    index_type L3>
+        class impl {
+        public:
+            
+            impl(): mat_(std::vector<std::vector<std::vector<spin_rep_t>>>
+                        (L1, std::vector<std::vector<spin_rep_t>>
+                        (L2, std::vector<spin_rep_t>(L3)))) {};
+            
+            void set(   index_type const & l1,
+                        index_type const & l2,
+                        index_type const & l3, 
+                        spin_ret_type const & s) {
+                mat_.at(l1).at(l2).at(l3) = s;
+            }
+            spin_ret_type const & get(  index_type const & l1,
+                                        index_type const & l2,
+                                        index_type const & l3) const {
+                return mat_.at(l1).at(l2).at(l3);
+            }
+        private:
+            system_t mat_;
+        };
+        
+    }; // struct baseline_greschd_matrix
+    
+}//end namespace mc_potts
+
+#endif //___HEADER
+
